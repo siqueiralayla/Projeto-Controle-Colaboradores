66d7f6f0938a19837d052cc30db72ca1362bf480
siqueiralayla <99771364+siqueiralayla@users.noreply.github.com>
2025-02-17 18:05:58 -0300

    Projeto Iniciado

---
+++ colaboradores/colaboradores/src/main/java/com/layla/colaboradores/ColaboradoresApplication.java
@@ -0,0 +1,13 @@
+package com.layla.colaboradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaboradoresApplication.class, args);
+	}
+
+}
---
+++ colaboradores/colaboradores/src/test/java/com/layla/colaboradores/ColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.layla.colaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
