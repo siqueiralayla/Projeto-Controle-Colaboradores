29ed66c6cf70899cc301bf896128c220d9f37ffc
Layla Siqueira <siqueiralayla@hotmail.com>
2025-04-04 12:04:40 -0300

    Inserindo validação das entidades

--- colaboradores/colaboradores/src/main/java/com/layla/colaboradores/entity/Cargo.java
+++ colaboradores/colaboradores/src/main/java/com/layla/colaboradores/entity/Cargo.java
@@ -8,6 +8,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 
 @SuppressWarnings("serial")
@@ -15,12 +18,17 @@ import jakarta.persistence.Table;
 @Table(name = "CARGOS")
 public class Cargo extends AbstractEntity<Long> {
 
+    @NotBlank(message = "O nome do cargo é obrigatório.")
+    @Size(max = 60, message = "O nome do cargo deve conter no máximo 60 caracteres.")
     @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
+    @NotNull(message = "Selecione o departamento relativo ao cargo.")
     @ManyToOne
     @JoinColumn(name = "id_departamento_fk")
     private Departamento departamento;
+
+
 
     @OneToMany(mappedBy = "cargo")
     private List<Funcionario> funcionarios;
